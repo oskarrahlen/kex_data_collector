cb85d3968020ea9e3681d62468cfae039cec1a7c
Sacharias Sjöqvist <sacharias.sjoqvist@gmail.com>
2019-02-26 11:38:24 +0100

    done v1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,121 @@
-import React, { Component } from 'react';
-import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
-import logo from './logo.svg';
-import './App.css';
-import ImageComponent from './components/ImageComponent';
-const uuidv4 = require('uuid/v4');
+import React, { Component } from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import "./App.css";
+import ImageComponent from "./components/ImageComponent";
+import { Button } from "antd";
+const uuidv4 = require("uuid/v4");
 
+const START_SCREEN = "START_SCREEN";
+const GOOD_SCREEN = "GOOD_SCREEN";
+const BAD_SCREEN = "BAD_SCREEN";
+const END_SCREEN = "END_SCREEN";
 
 class App extends Component {
-
   state = {
-    hash:""
-  }
-
+    hash: "",
+    screen: START_SCREEN,
+    dotLocation: null
+  };
 
   makeid() {
-   return uuidv4();
+    return uuidv4();
   }
+  componentDidMount() {
+    this.setState({ hash: this.makeid() });
+  }
+  handleClick = e => {
+    e.preventDefault();
+    this.setState({ screen: GOOD_SCREEN, dotLocation: 1 });
+  };
+  afterCapture = () => {
+    console.log("photo taken");
+    const { screen, dotLocation } = this.state;
 
-  componentDidMount(){
-    this.setState({hash:this.makeid()})
-  }
- 
-  
+    if (dotLocation >= 5 && screen === GOOD_SCREEN) {
+      // Change to bad screen
+      this.setState({ dotLocation: 1, screen: BAD_SCREEN });
+    } else if (dotLocation >= 5 && screen === BAD_SCREEN) {
+      // Change to end screen
+      this.setState({ dotLocation: null, screen: END_SCREEN });
+    } else {
+      this.setState({ dotLocation: dotLocation + 1 });
+    }
+  };
+  renderDot = () => {
+    const { dotLocation } = this.state;
+    if (dotLocation === 1)
+      return <div className="dot" style={{ left: 0, top: 0 }} />;
+    if (dotLocation === 2)
+      return <div className="dot" style={{ right: 0, top: 0 }} />;
+    if (dotLocation === 3)
+      return <div className="dot" style={{ left: 0, bottom: 0 }} />;
+    if (dotLocation === 4)
+      return <div className="dot" style={{ right: 0, bottom: 0 }} />;
+    if (dotLocation === 5)
+      return (
+        <div
+          className="dot"
+          style={{ left: "50%", right: "50%", top: "50%" }}
+        />
+      );
+
+    return null;
+  };
+
   render() {
+    const { screen } = this.state;
+
     return (
-      <div className="App">
-        <ImageComponent folder="oskarsbilder/" filename="bad1" hash={this.state.hash} />
+      <div className="app">
+        {this.renderDot()}
+        <div className="sidebar">
+          <h2 class="title">Instruktioner</h2>
+          Instruktioner, Instruktioner, 1. Instruktioner
+        </div>
+        <div className="main">
+          {screen === START_SCREEN && (
+            <div>
+              <h2>Välkommen</h2>
+              <div className="information">
+                Information Information Information
+              </div>
+              <Button type="primary" onClick={this.handleClick}>
+                Starta
+              </Button>
+            </div>
+          )}
+          {screen === GOOD_SCREEN && (
+            <div className="photo-view">
+              <h2>Ergonomiskt foto</h2>
+              <p>Text om detta</p>
+              <ImageComponent
+                folder="oskarsbilder/"
+                filename="good1"
+                hash={this.state.hash}
+                afterCapture={this.afterCapture}
+              />
+            </div>
+          )}
+          {screen === BAD_SCREEN && (
+            <div className="photo-view">
+              <h2>Dålig hållning</h2>
+              <ImageComponent
+                folder="oskarsbilder/"
+                filename="bad1"
+                hash={this.state.hash}
+                afterCapture={this.afterCapture}
+              />
+            </div>
+          )}
+          {screen === END_SCREEN && (
+            <div>
+              <h2>Slutfört</h2>
+              <div className="information">
+                Tack för din tid. Uppdatera sidan för att göra om testet.
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
-import { Button } from 'antd';
+import React, { Component } from "react";
+import { Button } from "antd";
 import Webcam from "react-webcam";
 
-
-
-
 class ImageComponent extends Component {
- 
   setRef = webcam => {
     this.webcam = webcam;
   };
 
   capture = () => {
     const imageSrc = this.webcam.getScreenshot();
-    fetch("https://31s16yvxie.execute-api.eu-central-1.amazonaws.com/default/uploadToS3", {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.      
-      body: JSON.stringify({
-        data: imageSrc,
-        imageName:this.props.folder + this.props.filename + "-" + this.props.hash
+    fetch(
+      "https://31s16yvxie.execute-api.eu-central-1.amazonaws.com/default/uploadToS3",
+      {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        body: JSON.stringify({
+          data: imageSrc,
+          imageName:
+            this.props.folder + this.props.filename + "-" + this.props.hash
+        })
+      }
+    )
+      .then(res => {
+        console.log(res);
       })
-    }).then((res) => {
-        console.log(res)
-      }).catch((err) => {
-        console.log(err)
-      })
-  
+      .catch(err => {
+        console.log(err);
+      });
+    this.props.afterCapture();
   };
- 
-  
+
   render() {
     const videoConstraints = {
       width: 1280,
@@ -36,18 +37,22 @@ class ImageComponent extends Component {
     };
 
     return (
-      <div >
-        <div>
-          <Webcam
-            audio={false}
-            height={600}
-            ref={this.setRef}
-            screenshotFormat="image/jpeg"
-            width={1000}
-            videoConstraints={videoConstraints}
-          />
-          <Button type="primary" onClick={()=>{this.capture()}}> Ta foto </Button>
-        </div>
+      <div className="camera">
+        <Webcam
+          audio={false}
+          height={180}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={200}
+          videoConstraints={videoConstraints}
+        />
+        <Button
+          onClick={() => {
+            this.capture();
+          }}
+        >
+          Ta foto
+        </Button>
       </div>
     );
   }
